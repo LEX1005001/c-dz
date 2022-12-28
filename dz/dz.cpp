8e065bbc5c622c9cf2eb0f3461e9912500348bfe
LEX1005001 <AleX69.galkin@yandex.ru>
2022-12-28 19:22:21 +0300

    Добавьте файлы проекта.

---
+++ dz/dz.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	float R1 = 0;
+	float R2 = 0;
+	float R3 = 0;
+	cout << "R1:";
+		cin >> R1;
+	cout << "R2:";
+		cin >> R2;
+	cout << "R3:";
+	cin >> R3;
+
+	cout<< "R0="<< 1 / R1 + 1 / R2 + 1 / R3;
+
+}
