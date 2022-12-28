4f2e97496f1b1d444b54a2828fa33c5cabf35bfa
LEX1005001 <AleX69.galkin@yandex.ru>
2022-12-28 19:48:23 +0300

    задание 2

--- dz/dz.cpp
+++ dz/dz.cpp
@@ -1,16 +1,14 @@
 ﻿#include <iostream>
 using namespace std;
 int main() {
-	float R1 = 0;
-	float R2 = 0;
-	float R3 = 0;
-	cout << "R1:";
-		cin >> R1;
-	cout << "R2:";
-		cin >> R2;
-	cout << "R3:";
-	cin >> R3;
+	float L = 0;
+	float pi = 3.14;
+	float R = 0;
 
-	cout<< "R0="<< 1 / R1 + 1 / R2 + 1 / R3;
-
+	cout << "L:";
+		cin >> L;
+	
+	R = L / (2 * pi);
+	
+	cout << "S=" << 2 * pi * (R*R);
 }
