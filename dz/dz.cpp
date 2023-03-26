cd8fcdc4f93796831afad4ee108888a68bab1fa7
LEX1005001 <117380414+LEX1005001@users.noreply.github.com>
2023-03-26 15:13:33 +0300

    Update dz.cpp

--- dz/dz.cpp
+++ dz/dz.cpp
@@ -1,14 +1,45 @@
-﻿#include <iostream>
+﻿#include<iostream>
+#include<cmath>
 using namespace std;
-int main() {
-	float L = 0;
-	float pi = 3.14;
-	float R = 0;
 
-	cout << "L:";
-		cin >> L;
-	
-	R = L / (2 * pi);
-	
-	cout << "S=" << 2 * pi * (R*R);
+int main()
+{
+    setlocale(0, "ru");
+    cout << "Введите первое число:";
+    int n1;
+    cin >> n1;
+    cout << "Введите втрое число:";
+    int n2;
+    cin >> n2;
+    cout << "Деляться без остатка на: " << endl;
+
+    if (n1 > n2)
+    {
+        for (int i = -n2; i <= n1; i++)
+        {
+            if (i == 0)
+            {
+                continue;
+            }
+            if (n1 % i == 0||n2 % i == 0)
+            {
+                cout << i << ' ';
+            }
+        }
+    }
+
+    if (n2 > n1)
+    {
+        for (int i = -n1; i <= n2; i++)
+        {
+            if (i == 0)
+            {
+                continue;
+            }
+            if (n1 % i == 0 || n2 % i == 0)
+            {
+                cout << i << ' ';
+            }
+        }
+    }
 }
